edb78a579de269781bb98caa47c19af174d23a05
kevin <kienvm.dev@gmail.com>
2021-02-01 07:37:36 +0700

    31-1

--- resources/views/livewire/post-search.blade.php
+++ resources/views/livewire/post-search.blade.php
@@ -11,10 +11,6 @@
         wire:keydown.enter="selectPost"
     />
 
-    <div wire:loading class="absolute z-10 list-group bg-white w-full rounded-t-none shadow-lg">
-        <div class="list-item">Searching...</div>
-    </div>
-
     @if(!empty($query))
         <div class="fixed top-0 right-0 bottom-0 left-0" wire:click="reset"></div>
 
@@ -29,6 +25,9 @@
             @else
                 <div class="list-item">No results!</div>
             @endif
+                <div wire:loading class="absolute z-10 list-group bg-white w-full rounded-t-none shadow-lg">
+                    <div class="list-item">Searching...</div>
+                </div>
         </div>
     @endif
 </div>
--- resources/views/livewire/posts/posts.blade.php
+++ resources/views/livewire/posts/posts.blade.php
@@ -16,16 +16,15 @@
                     </div>
                 </div>
             @endif
-            <div class="bg-white px-4 pt-5 pb-4">
+            <div class="bg-white px-4">
                 <livewire:post-search />
             </div>
-            @if (Request::getPathInfo() == '/dashboard/posts')
+            <div class="bg-white px-4 pt-5 pb-4">
                 <button wire:click="create()"
-                    class="inline-flex items-center px-4 py-2 my-3 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+                    class="inline-flex items-center px-4 py-2 my-3 bg-green-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-700 active:bg-green-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
                     Create New Post
                 </button>
-            @endif
-
+            </div>
             @if ($isOpen)
                 @include('livewire.posts.create')
             @endif
@@ -33,23 +32,35 @@
                 @foreach ($posts as $post)
                     <div class="max-w-sm rounded overflow-hidden shadow-lg">
                         <div class="px-6 py-4">
-                            <div class="font-bold text-xl mb-2">{{ $post->title }}</div>
+                            <div class="font-bold text-xl mb-2">
+                                <a href="{{ url('dashboard/posts', $post->id) }}">{{ $post->title }}</a>
+                            </div>
                             <p class="text-gray-700 text-base">
                                 {{ Str::words($post->content, 20, '...') }}
                             </p>
                         </div>
                         <div class="px-6 pt-4 pb-2">
-                            <a href="{{ url('dashboard/posts', $post->id) }}"
-                                class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
-                                Read post
-                            </a>
                             <button wire:click="edit({{ $post->id }})"
-                                class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
-                                Edit
+                                class="inline-flex items-center px-4 py-2 bg-yellow-400 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-500 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+                                <svg class="h-6 w-6 text-white-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                                    />
+                                </svg>
                             </button>
                             <button wire:click="delete({{ $post->id }})"
                                 class="inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red active:bg-red-600 transition ease-in-out duration-150">
-                                Delete
+                                <svg class="h-6 w-6 text-white-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                                    />
+                                </svg>
                             </button>
                         </div>
 
